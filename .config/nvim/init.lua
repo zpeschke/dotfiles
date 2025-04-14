4fce2375373d8ccc8743f80bce0ee6ac95fc1e6d
Zachary Peschke <zac@zulium.com>
2025-04-14 10:52:04 -0700

    nvim: add keybind for telescope grep of current word

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,6 +98,9 @@ vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fw', function()
+  telescope.live_grep({ default_text = vim.fn.expand('<cWORD>') })
+end, { desc = 'Telescope live grep current word' })
 
 require("oil").setup({
   keymaps = {
