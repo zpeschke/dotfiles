806045f00ed11e3ef46a3296a2a185895fb93773
Zac Peschke <zac@zulium.com>
2025-05-16 06:56:15 -0700

    Add telescope keybinds for both relative and absolute searching

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,38 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local telescope = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>tf', telescope.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>tg', telescope.live_grep, { desc = 'Telescope live grep' })
+
+    local get_cwd = function()
+      local path = vim.fn.expand('%:p:h')
+
+      -- oil.nvim prepends buffer path URLs with 'oil://', which causes
+      -- telescoped being invoked from oil.nvim using the 'cwd' arg not to work.
+      if path:match('^oil://') then
+        path = path:gsub('^oil://', '')
+      end
+
+      return path
+    end
+
+    -- relative file search
+    vim.keymap.set('n', '<leader>tf', function()
+      telescope.find_files({ cwd = get_cwd() })
+    end, { desc = 'Telescope find files' })
+
+    -- global file search from where buffer was originally opened
+    vim.keymap.set('n', '<leader>tF', telescope.find_files, { desc = 'Telescope find files' })
+
+    -- relative grep
+    vim.keymap.set('n', '<leader>tg', function()
+      telescope.live_grep({ cwd = get_cwd() })
+    end, { desc = 'Telescope live grep' })
+
+    -- global grep from where buffer was originally opened
+    vim.keymap.set('n', '<leader>tG', telescope.live_grep, { desc = 'Telescope live grep' })
+
     vim.keymap.set('n', '<leader>tb', telescope.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>th', telescope.help_tags, { desc = 'Telescope help tags' })
+
     vim.keymap.set('n', '<leader>tw', function()
       telescope.live_grep({ default_text = vim.fn.expand('<cWORD>') })
     end, { desc = 'Telescope live grep current word' })
