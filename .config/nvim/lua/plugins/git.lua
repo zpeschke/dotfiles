36ff393ba2e1e7309c777d8edd42b24c4634b02b
Zac Peschke <zac@zulium.com>
2025-07-17 22:35:26 -0700

    Add neogit to nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,10 +1,24 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  version = "*",
-  opts = {
-    on_attach = function(bufnr)
-      local gitsigns = require("gitsigns")
-      vim.keymap.set('n', '<leader>gb', gitsigns.blame, { desc = 'Gitsigns toggle line blame' })
-    end
+  {
+    'lewis6991/gitsigns.nvim',
+    version = '*',
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+        vim.keymap.set('n', '<leader>gb', gitsigns.blame, { desc = 'Gitsigns toggle line blame' })
+      end
+    },
+  },
+  {
+    'NeogitOrg/neogit',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'sindrets/diffview.nvim',
+        'nvim-telescope/telescope.nvim',
+      },
+      config = function()
+        local neogit = require('neogit')
+        vim.keymap.set('n', '<leader>gg', neogit.open, { desc = 'Open Neogit' })
+      end,
   }
 }
