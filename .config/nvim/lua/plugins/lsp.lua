a610c4675e4532ae790a34e23d49d083ec234ef4
Zac Peschke <zac@zulium.com>
2025-12-05 11:31:01 -0700

    fix(nvim): use vim.lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,25 +3,16 @@ return {
   version = "*",
   config = function()
     local lspconfig = require("lspconfig")
-    local configs = require("lspconfig.configs")
 
-    -- Enable Go LSP
-    lspconfig.gopls.setup({})
+    vim.lsp.enable("gopls")
+    vim.lsp.enable("terraformls")
 
-    -- Enable Terraform LSP
-    lspconfig.terraformls.setup({})
-
-    -- Configure Jsonnet LSP if one doesn't already exist
-    configs.jsonnet = {
-      default_config = {
-        cmd = {"jsonnet-language-server", "-t"},
-        filetypes = {"jsonnet", "libsonnet"},
-        root_dir = lspconfig.util.root_pattern(".git"),
-        settings = {},
-      }
-    }
-
-    -- Enable Jsonnet LSP
-    lspconfig.jsonnet.setup({})
+    vim.lsp.config("jsonnet", {
+      cmd = {"jsonnet-language-server", "-t"},
+      filetypes = {"jsonnet", "libsonnet"},
+      root_dir = lspconfig.util.root_pattern(".git"),
+      settings = {},
+    })
+    vim.lsp.enable("jsonnet")
   end,
 }
