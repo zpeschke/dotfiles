e95f76bc19e15a055ffe512f9009027c6b643372
Zac Peschke <zac@zulium.com>
2026-09-04 08:13:36 -0700

    fix(nvim): repair broken LSP config
    Use of lspconfig.util.root_pattern() is an old sync function that caused
    root_dir to never call back, so LSP client silently never started. Using
    async function(bufnr, on_dir) resolves it.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,16 +2,16 @@ return {
   "neovim/nvim-lspconfig",
   version = "*",
   config = function()
-    local lspconfig = require("lspconfig")
-
     vim.lsp.enable("gopls")
     vim.lsp.enable("pylsp")
     vim.lsp.enable("terraformls")
 
     vim.lsp.config("jsonnet", {
       cmd = {"jsonnet-language-server", "-t"},
-      filetypes = {"jsonnet", "libsonnet"},
-      root_dir = lspconfig.util.root_pattern(".git"),
+      filetypes = {"jsonnet"},
+      root_dir = function(bufnr, on_dir)
+        on_dir(vim.fs.root(bufnr, ".git"))
+      end,
       settings = {},
     })
     vim.lsp.enable("jsonnet")
